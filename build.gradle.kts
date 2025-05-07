c85b9ef7a14a00f7901724c66774db4853013b3e
Yan F <zhirnitoni@gmail.com>
2025-05-07 10:58:19 +0300

    Initial project setup with .*ignore files, docker files and logging settings. Also, with portable gradle wrapper included.
    `./deploy` directory and `deploy.properties` file are added as a cheap  environment specific configuration solution. CAN be superseded by Spring Cloud Configuration, but I decided to avoid setting up multiproject for the tryout purposes.

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.4.5"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    //В качестве дискавери сервиса использую Consul.
+    implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
---
+++ src/main/java/com/github/cvazer/tryout/webrise/Application.java
@@ -0,0 +1,15 @@
+package com.github.cvazer.tryout.webrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = "classpath:deploy.properties", ignoreResourceNotFound = true)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
